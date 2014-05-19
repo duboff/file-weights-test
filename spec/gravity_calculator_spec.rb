111776305553bedd3aab00c63e8cfbd8a8ec9fff
Mikhail Dubov <mdubov@gmail.com>
2014-05-19 23:53:44 +0100

    Added more tests for the calculator

--- spec/gravity_calculator_spec.rb
+++ spec/gravity_calculator_spec.rb
@@ -9,4 +9,25 @@ describe GravityCalculator do
   it 'correctly assigns weight to songs' do
     expect(calc.factor(:song)).to eq 1.2
   end
+
+  it 'correctly assigns weight to videos' do
+    expect(calc.factor(:video)).to eq 1.4
+  end
+
+  it 'correctly assigns weight to documents' do
+    expect(calc.factor(:document)).to eq 1.1
+  end
+
+  it 'correctly assigns weight to songs' do
+    expect(calc.factor(:binary)).to eq 1
+  end
+
+  it 'correctly assigns weight to songs' do
+    expect(calc.factor(:text)).to eq 1
+  end
+
+  it 'correctly assigns weight to songs' do
+    expect(calc.factor(:other)).to eq 1
+  end
+
 end
